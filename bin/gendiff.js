9a5be8b973d563e1f08fefc5c0e9d8bbe2af92b6
Georgiy Melikhov <g.melihov@electroneek.com>
2021-01-22 00:03:19 +0300

    create first gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
+import program from 'commander';
 
-console.log('gendiff');
+program
+  .version('1.0.0')
+  .description('Compares two configuration files and shows a difference.');
+program.parse();
