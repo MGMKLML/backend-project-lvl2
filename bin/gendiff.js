5c96f0668b8cbae95a17013d1a9190439cdf725e
Georgiy Melikhov <g.melihov@electroneek.com>
2021-01-22 01:18:47 +0300

    add format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,5 +3,7 @@ import program from 'commander';
 
 program
   .version('1.0.0')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filePath1> <filePath2>')
+  .option('-f, --format [type]', 'output format');
 program.parse();
