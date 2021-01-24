f0d3c9aac94f70f6109fd7dac538882fd75b1bfd
MGMKLML <mgmklml@yandex.ru>
2021-01-24 21:22:58 +0300

    create tests for plain jsons

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,25 @@
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import path, { dirname } from 'path';
 import genDiff from '../index.js';
-import { test, expect } from '@jest/globals';
 
-test('first', () => {
-    expect(3).toBe(3);
-})
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFilePath = (fileName) => path.join(__dirname, '__fixtures__', fileName);
+
+describe('plain json diff', () => {
+  test('two different files', () => {
+    const plainJson1 = getFilePath('plain_1.json');
+    const plainJson2 = getFilePath('plain_2.json');
+    const plainResult = fs.readFileSync(getFilePath('two_plain_jsons_res.txt')).toString();
+
+    expect(genDiff(plainJson1, plainJson2)).toBe(plainResult);
+  });
+
+  test('same file', () => {
+    const plainJson = getFilePath('plain_1.json');
+    const plainResult = fs.readFileSync(getFilePath('same_plain_json_res.txt')).toString();
+    expect(genDiff(plainJson, plainJson)).toBe(plainResult);
+  });
+});
