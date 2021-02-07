f3c9a90df8bf280d5a1460e15d6ebe13e506cd1f
MGMKLML <mgmklml@yandex.ru>
2021-02-08 01:58:07 +0300

    fix no-let and no-mutation at plain formatter

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -7,34 +7,23 @@ const printValue = (value) => {
   return JSON.stringify(value).replace(/"/g, "'");
 };
 
-const print = (tree) => {
-  let str = '';
-  tree.forEach((element) => {
-    const {
-      path, type, value, before, after, children,
-    } = element;
-    switch (type) {
-      case 'changed':
-        str = `${str}Property '${path}' was updated. From ${printValue(before)} to ${printValue(after)}\n`;
-        break;
-      case 'removed':
-        str = `${str}Property '${path}' was removed\n`;
-        break;
-      case 'added':
-        str = `${str}Property '${path}' was added with value: ${printValue(value)}\n`;
-        break;
-      case 'parent':
-        str = `${str}${print(children)}`;
-        break;
-      case 'same':
-      default:
-        break;
-    }
-  });
-  return str;
-};
+const print = (tree) => tree.flatMap((element) => {
+  const {
+    path, type, value, before, after, children,
+  } = element;
+  switch (type) {
+    case 'changed':
+      return `Property '${path}' was updated. From ${printValue(before)} to ${printValue(after)}`;
+    case 'removed':
+      return `Property '${path}' was removed`;
+    case 'added':
+      return `Property '${path}' was added with value: ${printValue(value)}`;
+    case 'parent':
+      return print(children);
+    case 'same':
+    default:
+      return null;
+  }
+}).filter((line) => !!line).join('\n');
 
-export default (tree) => {
-  const result = print(tree);
-  return result.replace(/\n*$/, '');
-};
+export default (tree) => print(tree);
